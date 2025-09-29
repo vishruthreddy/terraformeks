8d006908a5778af4fd671031a7750b7c3609d4b4
Your Name <you@example.com>
2025-09-29 13:58:07 -0500

    Initial commit of Terraform code

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,9 @@
-# Output the VPC ID (created or existing)
+# Output the VPC ID
 output "vpc_id" {
-  value = var.create_vpc && length(aws_vpc.this) > 0 ? aws_vpc.this[0].id : var.vpc_id
+  value = var.vpc_id
 }
 
-# Output the subnet IDs
+# Output the subnet IDs in the VPC
 output "subnet_ids" {
-  value = var.create_vpc && length(aws_subnet.this) > 0 ? aws_subnet.this[*].id : data.aws_subnets.existing[0].ids
+  value = data.aws_subnets.existing.ids
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,23 +1,6 @@
-variable "create_vpc" {
-  description = "Whether to create a new VPC or use an existing one"
-  type        = bool
-  default     = false
-}
-
+# Variables for existing VPC
 variable "vpc_id" {
-  description = "ID of an existing VPC to use (if create_vpc is false)"
+  description = "ID of the existing VPC to use"
   type        = string
-  default     = "vpc-5a863f32"  # Replace with your existing VPC ID
-}
-
-variable "cidr_block" {
-  description = "CIDR block for the VPC (if creating new)"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "azs" {
-  description = "Availability zones for subnets (if creating new VPC)"
-  type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = "vpc-5a863f32"
 }
