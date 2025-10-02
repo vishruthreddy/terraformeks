dfb43e11a9f84acaf180d475be90a0e1b4c8463c
Your Name <you@example.com>
2025-10-02 16:05:55 -0500

    Initial commit of Terraform code

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -15,6 +15,47 @@ variable "create_iam_roles" {
   type        = bool
   default     = false
 }
+
+# -------------------------
+# Combined policy for viewing EKS nodes + IAM management
+# -------------------------
+resource "aws_iam_policy" "eks_and_iam_policy" {
+  name        = "ViewEKSNodesAndManageIAMPolicy"
+  description = "Allows viewing EKS node groups, EC2 instances, and IAM policy management for Terraform"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          # EKS & EC2 describe actions
+          "eks:DescribeCluster",
+          "eks:ListNodegroups",
+          "eks:DescribeNodegroup",
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          
+          # IAM actions for policy management
+          "iam:CreatePolicy",
+          "iam:AttachUserPolicy",
+          "iam:GetPolicy",
+          "iam:ListAttachedUserPolicies"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the combined policy to the existing node role
+resource "aws_iam_role_policy_attachment" "attach_eks_and_iam_policy" {
+  role       = var.node_role_arn
+  policy_arn = aws_iam_policy.eks_and_iam_policy.arn
+}
+
 # -------------------------
 # Outputs for EKS module
 # -------------------------
