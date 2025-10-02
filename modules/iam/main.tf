5449d27598f179d5af061e9bdf9b47c686ae9d66
Your Name <you@example.com>
2025-10-02 15:03:03 -0500

    Initial commit of Terraform code

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -15,6 +15,40 @@ variable "create_iam_roles" {
   type        = bool
   default     = false
 }
+
+# -------------------------
+# Policy to allow viewing EKS nodes in console
+# -------------------------
+resource "aws_iam_policy" "view_eks_nodes" {
+  name        = "ViewEKSNodesPolicy"
+  description = "Allows viewing EKS node groups and EC2 instances for console visibility"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "eks:DescribeCluster",
+          "eks:ListNodegroups",
+          "eks:DescribeNodegroup",
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the policy to the existing node role
+resource "aws_iam_role_policy_attachment" "attach_view_eks_nodes" {
+  role       = var.node_role_arn
+  policy_arn = aws_iam_policy.view_eks_nodes.arn
+}
+
 # -------------------------
 # Outputs for EKS module
 # -------------------------
