8496f508ac686252dd4d1aced619ff3abe341695
Your Name <you@example.com>
2025-10-03 10:34:34 -0500

    Initial commit of Terraform code

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -15,6 +15,32 @@ variable "create_iam_roles" {
   type        = bool
   default     = false
 }
+resource "aws_iam_policy" "eks_nodegroup_asg_view" {
+  name        = "eks-nodegroup-asg-view"
+  description = "Allow EKS node group IAM role to view Auto Scaling Group for console"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:DescribeScalingInstances",
+          "autoscaling:DescribeLaunchConfigurations"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_asg_policy_to_node_role" {
+  role       = var.node_role_arn
+  policy_arn = aws_iam_policy.eks_nodegroup_asg_view.arn
+}
+
 # -------------------------
 # Outputs for EKS module
 # -------------------------
